e1f4465ee0b23b6b70a7b8bd51d7cc3da5c33d2d
ricanlonghorn23 <luis.ra.aldaz@gmail.com>
2024-07-31 15:04:52 -0500

    Second commit for hmwk

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,37 @@
 import { useQuery } from '@apollo/client';
-
-
-
+import Auth from '../utils/auth';
+import { Message, Button, Container, Divider, Grid, Header, Icon, Image, List, Menu, Segment, Sidebar, } from 'semantic-ui-react'
+import { useParams } from 'react-router-dom';
 import { QUERY_USER } from '../utils/queries';
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_USER);
   const user = data?.user || [];
+  
+  
 
   return (
-    <main>
-      <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-       
-        </div>
-      </div>
-    </main>
+    <div>
+    {Auth.loggedIn() ? (
+    <Segment style={{ padding: '8em 0em' }} vertical>
+    <Grid.Column>
+       <Message style={{ textAlign: 'center' }} error>This is the home page when the user logs in</Message>
+      </Grid.Column>
+  </Segment>
+) : (     <Segment style={{ padding: '8em 0em' }} vertical>
+   <Message info style={{ textAlign: 'center' }}>
+          Don't be gone to long your pet needs you!!
+        </Message>
+  <Grid container stackable verticalAlign='middle'>
+    <Grid.Row>
+      <Grid.Column textAlign='center'>
+        <Button size='Large' href='/' >Go to Login Page</Button>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+</Segment>
+)}
+</div>
   );
 };
 
